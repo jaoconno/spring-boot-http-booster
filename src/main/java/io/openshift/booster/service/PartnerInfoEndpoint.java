661d467921a575448895949eb3066e7e9e0bcb98
jaoconno <oconnorjena@gmail.com>
2019-04-05 19:23:56 -0400

    Create PartnerInfoEndpoint.java

---
+++ src/main/java/io/openshift/booster/service/PartnerInfoEndpoint.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016-2017 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.openshift.booster.service;
+
+import java.util.Arrays;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import org.springframework.stereotype.Component;
+
+@Path("/greeting")
+@Component
+public class PartnerInfoEndpoint {
+    @GET
+    @Produces("application/json")
+    public PartnerInfo greeting(@QueryParam("name") @DefaultValue("World") String name) {
+        PartnerInfo info = new PartnerInfo();
+        Accreditation accreditation = new Accreditation();
+        accreditation.setSpecialty("SALES");
+        accreditation.setSpecialization("TEST");
+        accreditation.setSalesType("TESTTYPE");
+        info.setAccreditations(Arrays.asList(accreditation));
+        info.setPartnerName(name);
+        return info;
+    }
+}
